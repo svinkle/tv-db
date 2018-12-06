c7a0ce364c5f7df76e74650d5cd2273b17bbe1d0
Scott Vinkle <scott.vinkle@shopify.com>
2018-12-06 07:36:17 -0500

    Updated postcss param requirements.

---
+++ build/precache-manifest.8c28af7188816fcc94d69dc89c5939c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6131cea4a7fb9fbc4461",
+    "url": "/static/js/main.6131cea4.chunk.js"
+  },
+  {
+    "revision": "31d98774e7f14fdf3bdc",
+    "url": "/static/js/1.31d98774.chunk.js"
+  },
+  {
+    "revision": "6131cea4a7fb9fbc4461",
+    "url": "/static/css/main.9452bc2b.chunk.css"
+  },
+  {
+    "revision": "31d98774e7f14fdf3bdc",
+    "url": "/static/css/1.6372255d.chunk.css"
+  },
+  {
+    "revision": "2432452493f3186e63bbca9ab9cd5c07",
+    "url": "/index.html"
+  }
+];
